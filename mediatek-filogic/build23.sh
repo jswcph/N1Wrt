478efcaf658b4ae8b0a64683b6b842c004e2a977
jswcph <84085592+jswcph@users.noreply.github.com>
2026-04-08 20:16:24 +0800

    Update build23.sh

--- mediatek-filogic/build23.sh
+++ mediatek-filogic/build23.sh
@@ -53,7 +53,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
-PACKAGES="$PACKAGES zram-config kmod-zram"
+PACKAGES="$PACKAGES kmod-zram"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
 PACKAGES="$PACKAGES luci-theme-argon"

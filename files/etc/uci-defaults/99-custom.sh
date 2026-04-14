c1b907c890adbdb79501aa5e86cac69db5f5b660
jswcph <84085592+jswcph@users.noreply.github.com>
2026-04-15 06:05:46 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -14,8 +14,8 @@ uci add dhcp domain
 uci set "dhcp.@domain[-1].name=time.android.com"
 uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
-# --- 修改这里：设置初始密码为 password ---
-echo "root:password" | chpasswd
+log_info "正在设置默认登录密码..."
+(echo "password"; sleep 1; echo "password") | passwd root >/dev/null 2>&1
 
 # 检查配置文件pppoe-settings是否存在 该文件由build.sh动态生成
 SETTINGS_FILE="/etc/config/pppoe-settings"
